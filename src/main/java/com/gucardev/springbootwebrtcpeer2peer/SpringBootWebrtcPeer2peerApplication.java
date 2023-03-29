f6e765e881bc82de4a97c1062169a2c5f1b0793f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-29 15:51:38 +0300

    project initialized

---
+++ src/main/java/com/gucardev/springbootwebrtcpeer2peer/SpringBootWebrtcPeer2peerApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootwebrtcpeer2peer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebrtcPeer2peerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootWebrtcPeer2peerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gucardev/springbootwebrtcpeer2peer/SpringBootWebrtcPeer2peerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootwebrtcpeer2peer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebrtcPeer2peerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
